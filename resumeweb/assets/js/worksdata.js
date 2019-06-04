4f67152eaacf0e7f4f554fa970bbf90267ba5933
B288 <sinhongchen@liontravel.com>
2019-06-04 13:39:46 +0800

    udpate

---
+++ resumeweb/assets/js/worksdata.js
@@ -0,0 +1,228 @@
+var worksdata = {
+    designBack: [{
+        title: "設計資源庫(後台)",
+        titleDesc: "管理和分享雄獅集團各網站元件、商標、圖片等等...",
+        caption: "設計資源庫(後台) - 首頁",
+        captionDesc: "你好",
+        imgName: "designBack.png",
+        class: "6u",
+        visible: true
+    }],
+    designFont: [{
+        title: "設計資源庫(前台)",
+        titleDesc: "管理和分享雄獅集團各網站元件、商標、圖片等等...",
+        caption: "設計資源庫(前台) - 首頁",
+        captionDesc: "可以直接輸入關鍵字查詢元件，或選擇的元件分類",
+        imgName: "designFont1.jpg",
+        class: "6u",
+        visible: true
+    }, {
+        title: "",
+        titleDesc: "",
+        caption: "設計資源庫(前台) - 登入",
+        captionDesc: "登入集團ERP系統之帳號密碼",
+        imgName: "designFont2.jpg",
+        class: "",
+        visible: false
+    }, {
+        title: "",
+        titleDesc: "",
+        caption: "設計資源庫(前台) - 下載預覽網頁控件",
+        captionDesc: "提供使用者下載或預覽網頁控件",
+        imgName: "designFont3.jpg",
+        class: "",
+        visible: false
+    }, {
+        title: "",
+        titleDesc: "",
+        caption: "設計資源庫(前台) - 雄好玩(吉祥物)圖片創建、下載",
+        captionDesc: "提供使用者下載雄獅吉祥物圖片、快速創建吉祥物圖入口",
+        imgName: "designFont4.jpg",
+        class: "",
+        visible: false
+    }, {
+        title: "",
+        titleDesc: "",
+        caption: "設計資源庫(前台) - 創建雄好玩(吉祥物)",
+        captionDesc: "利用部位圖片組成一張客製化的雄好玩(吉祥物)",
+        imgName: "designFont5.jpg",
+        class: "",
+        visible: false
+    }, {
+        title: "",
+        titleDesc: "",
+        caption: "設計資源庫(前台) - 暫存創建到一半的雄好玩(吉祥物)",
+        captionDesc: "暫存使用者所創建的雄好玩圖(吉祥物)，可繼續編輯",
+        imgName: "designFont7.jpg",
+        class: "",
+        visible: false
+    }, {
+        title: "",
+        titleDesc: "",
+        caption: "設計資源庫(前台) - 創建雄好玩(吉祥物)申請審核",
+        captionDesc: "審核使用者所創建的雄好玩圖(吉祥物)，才能供人下載",
+        imgName: "designFont8.jpg",
+        class: "",
+        visible: false
+    }, {
+        title: "",
+        titleDesc: "",
+        caption: "設計資源庫(前台) - 創建雄好玩(吉祥物)審核流程",
+        captionDesc: "審核使用者所創建的雄好玩圖(吉祥物)，才能供人下載",
+        imgName: "designFont6.jpg",
+        class: "",
+        visible: false
+    }, {
+        title: "",
+        titleDesc: "",
+        caption: "設計資源庫(前台) - 搜尋結果",
+        imgName: "designFont9.jpg",
+        class: "",
+        visible: false
+    }, {
+        title: "",
+        titleDesc: "",
+        caption: "設計資源庫(前台) - 訊息通知",
+        imgName: "designFont10.jpg",
+        class: "",
+        visible: false
+    }],
+    works: []
+        //{
+        //    title: "設計資源庫(前台)",
+        //    titleDesc: "管理和分享雄獅集團各網站元件、商標、圖片等等...",
+        //    caption: "設計資源庫(前台) - 首頁",
+        //    captionDesc: "可以直接輸入關鍵字查詢元件，或選擇的元件分類",
+        //    imgName: "designFont1.jpg",
+        //    class: "6u",
+        //    visible: true
+        //},
+        //{
+        //    title: "",
+        //    titleDesc: "",
+        //    caption: "設計資源庫(前台) - 登入",
+        //    imgName: "designFont2.jpg",
+        //    class: "",
+        //    visible: false
+        //},
+        //{
+        //    title: "",
+        //    titleDesc: "",
+        //    caption: "設計資源庫(前台) - 下載預覽網頁控件",
+        //    imgName: "designFont3.jpg",
+        //    class: "",
+        //    visible: false
+        //},
+        //{
+        //    title: "",
+        //    titleDesc: "",
+        //    caption: "設計資源庫(前台) - 雄好玩(吉祥物)圖片創建、下載",
+        //    imgName: "designFont4.jpg",
+        //    class: "",
+        //    visible: false
+        //},
+        //{
+        //    title: "",
+        //    titleDesc: "",
+        //    caption: "設計資源庫(前台) - 創建雄好玩(吉祥物)",
+        //    imgName: "designFont5.jpg",
+        //    class: "",
+        //    visible: false
+        //},
+        //{
+        //    title: "",
+        //    titleDesc: "",
+        //    caption: "設計資源庫(前台) - 創建雄好玩(吉祥物)審核流程",
+        //    imgName: "designFont6.jpg",
+        //    class: "",
+        //    visible: false
+        //},
+        //{
+        //    title: "",
+        //    titleDesc: "",
+        //    caption: "設計資源庫(前台) - 暫存創建到一半的雄好玩(吉祥物)",
+        //    imgName: "designFont7.jpg",
+        //    class: "",
+        //    visible: false
+        //},
+        //{
+        //    title: "",
+        //    titleDesc: "",
+        //    caption: "設計資源庫(前台) - 創建雄好玩(吉祥物)申請審核",
+        //    imgName: "designFont8.jpg",
+        //    class: "",
+        //    visible: false
+        //},
+        //{
+        //    title: "",
+        //    titleDesc: "",
+        //    caption: "設計資源庫(前台) - 搜尋結果",
+        //    imgName: "designFont9.jpg",
+        //    class: "",
+        //    visible: false
+        //},
+        //{
+        //    title: "",
+        //    titleDesc: "",
+        //    caption: "設計資源庫(前台) - 訊息通知",
+        //    imgName: "designFont10.jpg",
+        //    class: "",
+        //    visible: false
+        //},
+        //{
+        //    title: "設計資源庫(後台)",
+        //    titleDesc: "管理\"設計資源庫\"前台網站架構、人員權限、審核元件、公布消息等等...",
+        //    caption: "設計資源庫(後台) - 首頁",
+        //    imgName: "designBack.png",
+        //    class: "6u$",
+        //    visible: true
+        //},
+        //{
+        //    title: "MOSME摩思米(前後台)",
+        //    titleDesc: "教材題庫整合、測驗或練習、自訂組卷、班級管理等等...",
+        //    caption: "MOSME摩思米(前台) - 首頁",
+        //    imgName: "mosme.png",
+        //    class: "6u",
+        //    visible: true
+        //},
+        //{
+        //    title: "Hoogaa網",
+        //    titleDesc: "即時教師與學生互動測驗，即時顯示作答與排名，有競爭和趣味性",
+        //    caption: "Hoogaa網 - 首頁",
+        //    imgName: "hoogaa.png",
+        //    class: "6u$",
+        //    visible: true
+        //},
+        //{
+        //    title: "GLAD認證系統",
+        //    titleDesc: "認證管理系統",
+        //    caption: "GLAD認證系統 - 首頁",
+        //    imgName: "glad.png",
+        //    class: "6u",
+        //    visible: true
+        //},
+        //{
+        //    title: "台灣創新科技管理發展協會認證系統",
+        //    titleDesc: "認證管理系統",
+        //    caption: "台灣創新科技管理發展協會認證系統 - 首頁",
+        //    imgName: "itm.png",
+        //    class: "6u$",
+        //    visible: true
+        //},
+        //{
+        //    title: "JYiC勁園國際認證系統",
+        //    titleDesc: "認證管理系統(電腦軟體應用乙丙級)",
+        //    caption: "JYiC勁園國際認證系統 - 首頁",
+        //    imgName: "jyic.png",
+        //    class: "6u",
+        //    visible: true
+        //},
+        //{
+        //    title: "中華流通連鎖產學聯盟協會認證系統",
+        //    titleDesc: "認證管理系統",
+        //    caption: "中華流通連鎖產學聯盟協會認證系統 - 首頁",
+        //    imgName: "care.png",
+        //    class: "6u$",
+        //    visible: true
+        //},
+}
