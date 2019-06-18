575996d139329bd3bae0bf4cce1c41a2db6ad365
B288 <sinhongchen@liontravel.com>
2019-06-18 15:09:20 +0800

    update

--- resumeweb/assets/js/worksdata.js
+++ resumeweb/assets/js/worksdata.js
@@ -1,8 +1,8 @@
 var worksdata = {
     hoogaaTeacher: [{
-            title: "hoogaa 呼嘎(教師端)",
+            title: "hoogaa(教師端)",
             titleDesc: "即互動測驗，即時顯示作答與排名，提升競爭和趣味性",
-            caption: "hoogaa 呼嘎(教師端) - 產生PIN碼、等待學生進入",
+            caption: "hoogaa(教師端) - 產生PIN碼、等待學生進入",
             captionDesc: "產生PIN碼提供給學生輸入、檢視當前有誰進入競賽",
             imgName: "hoogaa-teacher1.jpg",
             class: "6u",
@@ -11,7 +11,7 @@ var worksdata = {
         {
             title: "",
             titleDesc: "",
-            caption: "hoogaa 呼嘎(教師端) - 開始考試倒數",
+            caption: "hoogaa(教師端) - 開始考試倒數",
             captionDesc: "由教師端統一開始進入競賽倒數",
             imgName: "hoogaa-teacher2.jpg",
             class: "",
@@ -20,7 +20,7 @@ var worksdata = {
         {
             title: "",
             titleDesc: "",
-            caption: "hoogaa 呼嘎(教師端) - 即時競賽結果",
+            caption: "hoogaa(教師端) - 即時競賽結果",
             captionDesc: "即時看考生作答情況、排名",
             imgName: "hoogaa-teacher3.jpg",
             class: "",
@@ -29,7 +29,7 @@ var worksdata = {
         {
             title: "",
             titleDesc: "",
-            caption: "hoogaa 呼嘎(教師端) - 競賽最終結果",
+            caption: "hoogaa(教師端) - 競賽最終結果",
             captionDesc: "考試結束後顯示考生答題資訊報表、排名及分數",
             imgName: "hoogaa-teacher4.jpg",
             class: "",
@@ -38,7 +38,7 @@ var worksdata = {
         {
             title: "",
             titleDesc: "",
-            caption: "hoogaa 呼嘎(教師端) - 試題解析",
+            caption: "hoogaa(教師端) - 試題解析",
             captionDesc: "讓老師可以檢討考題，也可以查看學生作答狀況",
             imgName: "hoogaa-teacher5.jpg",
             class: "",
@@ -46,9 +46,9 @@ var worksdata = {
         }
     ],
     hoogaaStudent: [{
-            title: "hoogaa 呼嘎(學生端)",
+            title: "hoogaa(學生端)",
             titleDesc: "即互動測驗，即時顯示作答與排名，提升競爭和趣味性",
-            caption: "hoogaa 呼嘎(學生端) - 輸入PIN碼",
+            caption: "hoogaa(學生端) - 輸入PIN碼",
             captionDesc: "學生輸入教師端所提供的代碼，即可進入等待競賽畫面",
             imgName: "hoogaa-student1.jpg",
             class: "6u",
@@ -57,7 +57,7 @@ var worksdata = {
         {
             title: "",
             titleDesc: "",
-            caption: "hoogaa 呼嘎(學生端) - 輸入名稱",
+            caption: "hoogaa(學生端) - 輸入名稱",
             captionDesc: "輸入名稱或備註，供辨別身分",
             imgName: "hoogaa-student2.jpg",
             class: "",
@@ -66,7 +66,7 @@ var worksdata = {
         {
             title: "",
             titleDesc: "",
-            caption: "hoogaa 呼嘎(學生端) - 等待進入競賽",
+            caption: "hoogaa(學生端) - 等待進入競賽",
             captionDesc: "輸入完基本資料後，進入等待考試畫面，等教師端統一開始競賽",
             imgName: "hoogaa-student3.jpg",
             class: "",
@@ -75,7 +75,7 @@ var worksdata = {
         {
             title: "",
             titleDesc: "",
-            caption: "hoogaa 呼嘎(學生端) - 作答畫面",
+            caption: "hoogaa(學生端) - 作答畫面",
             captionDesc: "開始作答，時間倒計時，作答完或是時間到會顯示正確解答",
             imgName: "hoogaa-student4.jpg",
             class: "",
@@ -84,7 +84,7 @@ var worksdata = {
         {
             title: "",
             titleDesc: "",
-            caption: "hoogaa 呼嘎(學生端) - 即時排名",
+            caption: "hoogaa(學生端) - 即時排名",
             captionDesc: "學生每作答完一題，會即時顯示當前分數及排名狀況",
             imgName: "hoogaa-student5.jpg",
             class: "",
@@ -93,7 +93,7 @@ var worksdata = {
         {
             title: "",
             titleDesc: "",
-            caption: "hoogaa 呼嘎(學生端) - 個人競賽結果",
+            caption: "hoogaa(學生端) - 個人競賽結果",
             captionDesc: "全部作答完後顯示競賽結果",
             imgName: "hoogaa-student6.jpg",
             class: "",
@@ -102,7 +102,7 @@ var worksdata = {
         {
             title: "",
             titleDesc: "",
-            caption: "hoogaa 呼嘎(學生端) - 查看競賽作答歷程",
+            caption: "hoogaa(學生端) - 查看競賽作答歷程",
             captionDesc: "顯示競賽作答歷程，學生可以看到作答時選擇的選項",
             imgName: "hoogaa-student7.jpg",
             class: "",
@@ -112,11 +112,59 @@ var worksdata = {
     designBack: [{
         title: "設計資源庫(後台)",
         titleDesc: "管理和分享雄獅集團各網站元件、商標、圖片等等...",
-        caption: "設計資源庫(後台) - 首頁",
-        captionDesc: "",
-        imgName: "designBack.png",
+        caption: "設計資源庫(後台) - 登入",
+        captionDesc: "登入公司ERP系統帳號密碼",
+        imgName: "designBack1.jpg",
         class: "6u",
         visible: true
+    }, {
+        title: "",
+        titleDesc: "",
+        caption: "設計資源庫(後台) - 首頁",
+        captionDesc: "公告操作(前台可以看到這裡新增的公告)",
+        imgName: "designBack2.jpg",
+        class: "",
+        visible: false
+    }, {
+        title: "",
+        titleDesc: "",
+        caption: "設計資源庫(後台) - 管理員設定",
+        captionDesc: "網站類別新增/修改，控制前台選單資料",
+        imgName: "designBack3.jpg",
+        class: "",
+        visible: false
+    }, {
+        title: "",
+        titleDesc: "",
+        caption: "設計資源庫(後台) - 管理員設定",
+        captionDesc: "需求單列表，審核前台上傳的圖片，審核通過才可以在前台顯示",
+        imgName: "designBack4.jpg",
+        class: "",
+        visible: false
+    }, {
+        title: "",
+        titleDesc: "",
+        caption: "設計資源庫(後台) - 管理員設定",
+        captionDesc: "使用者列表，管理使用者權限設定",
+        imgName: "designBack5.jpg",
+        class: "",
+        visible: false
+    }, {
+        title: "",
+        titleDesc: "",
+        caption: "設計資源庫(後台) - 上架內容列表(商標系統為例)",
+        captionDesc: "管理上架內容",
+        imgName: "designBack6.jpg",
+        class: "",
+        visible: false
+    }, {
+        title: "",
+        titleDesc: "",
+        caption: "設計資源庫(後台) - 編輯上架內容",
+        captionDesc: "編輯上架內容",
+        imgName: "designBack7.jpg",
+        class: "",
+        visible: false
     }],
     designFont: [{
         title: "設計資源庫(前台)",
