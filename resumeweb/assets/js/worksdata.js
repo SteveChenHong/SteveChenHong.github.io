f7f9f0b49b5c4f863913abe7b3e4c75367f25db7
Steve <Steve@DESKTOP-VU7NHK7>
2019-06-22 16:03:18 +0800

    update

--- resumeweb/assets/js/worksdata.js
+++ resumeweb/assets/js/worksdata.js
@@ -1,477 +1,228 @@
 var worksdata = {
     lionbobby: [{
-        title: "雄保旅遊網",
-        titleDesc: "旅遊產品購買、報名",
-        caption: "雄保旅遊網 - 首頁",
-        captionDesc: "",
-        imgName: "lionbobby1.jpg",
-        class: "6u",
-        visible: true
-    }, {
-        title: "",
-        titleDesc: "",
         caption: "雄保旅遊網 - 搜尋結果頁",
         captionDesc: "顯示產品列表、包含月曆選團、比較、加入收藏、看過紀錄等功能",
-        imgName: "lionbobby2.jpg",
-        class: "",
-        visible: false
+        imgName: "lionbobby2.jpg"
     }],
     join: [{
-            title: "加盟平台",
-            titleDesc: "提供旅天下的加盟商使用的後台系統",
-            caption: "加盟平台 - 登入畫面",
-            captionDesc: "輸入帳號密碼、一次性密碼(OTP)進行登入",
-            imgName: "join1.jpg",
-            class: "6u",
-            visible: true
-        }, {
-            title: "",
-            titleDesc: "",
             caption: "加盟平台 - 首頁",
             captionDesc: "查看最新系統留言、訂單留言、其他登入者資訊",
-            imgName: "join2.jpg",
-            class: "",
-            visible: false
+            imgName: "join2.jpg"
         }, {
-            title: "",
-            titleDesc: "",
             caption: "加盟平台 - 團體銷售",
             captionDesc: "顯示團體產品列表",
-            imgName: "join3.jpg",
-            class: "",
-            visible: false
+            imgName: "join3.jpg"
         }, {
-            title: "",
-            titleDesc: "",
             caption: "加盟平台 - 確認產品",
             captionDesc: "確認選購的產品、選擇參團人數",
-            imgName: "join6.jpg",
-            class: "",
-            visible: false
+            imgName: "join6.jpg"
         }, {
-            title: "",
-            titleDesc: "",
             caption: "加盟平台 - 填寫旅客資料",
             captionDesc: "填寫旅客資料",
-            imgName: "join7.jpg",
-            class: "",
-            visible: false
+            imgName: "join7.jpg"
         }, {
-            title: "",
-            titleDesc: "",
             caption: "加盟平台 - 訂單列表",
             captionDesc: "查看訂單列表",
-            imgName: "join4.jpg",
-            class: "",
-            visible: false
+            imgName: "join4.jpg"
         }, {
-            title: "",
-            titleDesc: "",
+
             caption: "加盟平台 - 訂單明細",
             captionDesc: "查看訂單明細、處理訂單相關功能",
-            imgName: "join8.jpg",
-            class: "",
-            visible: false
+            imgName: "join8.jpg"
         },
         {
-            title: "",
-            titleDesc: "",
             caption: "加盟平台 - 門店應付",
             captionDesc: "查看門店應付帳款等資訊",
-            imgName: "join9.jpg",
-            class: "",
-            visible: false
+            imgName: "join9.jpg"
         }, {
-            title: "",
-            titleDesc: "",
             caption: "加盟平台 - 收退款審核",
             captionDesc: "處理收退款審核作業",
-            imgName: "join5.jpg",
-            class: "",
-            visible: false
+            imgName: "join5.jpg"
         }
     ],
     mosme: [{
-            title: "Mosme摩斯米",
-            titleDesc: "教材題庫整合、測驗或練習、自訂組卷、班級管理等等...",
-            caption: "Mosme摩斯米 - 首頁",
-            captionDesc: "輸入關鍵字可快速搜尋相關書籍資料",
-            imgName: "mosme1.jpg",
-            class: "6u",
-            visible: true
-        },
-        {
-            title: "",
-            titleDesc: "",
             caption: "Mosme摩斯米 - 書籍列表",
             captionDesc: "顯示書籍產品",
-            imgName: "mosme2.jpg",
-            class: "",
-            visible: false
+            imgName: "mosme2.jpg"
         },
         {
-            title: "",
-            titleDesc: "",
             caption: "Mosme摩斯米 - 書籍詳細資料",
             captionDesc: "顯示書籍簡介、題庫練習功能、書籍開通功能",
-            imgName: "mosme3.jpg",
-            class: "",
-            visible: false
+            imgName: "mosme3.jpg"
         },
         {
-            title: "",
-            titleDesc: "",
             caption: "Mosme摩斯米 - 我的書單",
             captionDesc: "顯示個人開通過的書籍",
-            imgName: "mosme4.jpg",
-            class: "",
-            visible: false
+            imgName: "mosme4.jpg"
         },
         {
-            title: "",
-            titleDesc: "",
             caption: "Mosme摩斯米 - 開通審核",
             captionDesc: "提供業務處理書籍開通、查詢開通紀錄等功能",
-            imgName: "mosme5.jpg",
-            class: "",
-            visible: false
+            imgName: "mosme5.jpg"
         },
         {
-            title: "",
-            titleDesc: "",
             caption: "Mosme摩斯米 - 課程管理",
             captionDesc: "提供教師對課程綁定相關書籍，將學生加入課程、發布公告、上傳教材、組卷等功能",
-            imgName: "mosme6.jpg",
-            class: "",
-            visible: false
+            imgName: "mosme6.jpg"
         },
         {
-            title: "",
-            titleDesc: "",
             caption: "Mosme摩斯米 - 組卷流程1",
             captionDesc: "選擇組卷題庫",
-            imgName: "mosme7.jpg",
-            class: "",
-            visible: false
+            imgName: "mosme7.jpg"
         },
         {
-            title: "",
-            titleDesc: "",
             caption: "Mosme摩斯米 - 組卷流程2",
             captionDesc: "選擇組卷考試範圍",
-            imgName: "mosme8.jpg",
-            class: "",
-            visible: false
+            imgName: "mosme8.jpg"
         },
         {
-            title: "",
-            titleDesc: "",
             caption: "Mosme摩斯米 - 組卷流程3",
             captionDesc: "選擇組卷題型、題數、配分",
-            imgName: "mosme9.jpg",
-            class: "",
-            visible: false
+            imgName: "mosme9.jpg"
         },
         {
-            title: "",
-            titleDesc: "",
             caption: "Mosme摩斯米 - 組卷流程4",
             captionDesc: "選擇組卷章節配題目",
-            imgName: "mosme10.jpg",
-            class: "",
-            visible: false
+            imgName: "mosme10.jpg"
         },
         {
-            title: "",
-            titleDesc: "",
             caption: "Mosme摩斯米 - 組卷流程5",
             captionDesc: "設定試卷詳細資料",
-            imgName: "mosme11.jpg",
-            class: "",
-            visible: false
+            imgName: "mosme11.jpg"
         },
         {
-            title: "",
-            titleDesc: "",
             caption: "Mosme摩斯米 - 題目預覽",
             captionDesc: "",
-            imgName: "mosme12.jpg",
-            class: "",
-            visible: false
+            imgName: "mosme12.jpg"
         },
         {
-            title: "",
-            titleDesc: "",
             caption: "Mosme摩斯米 - 匯出試卷",
             captionDesc: "",
-            imgName: "mosme13.jpg",
-            class: "",
-            visible: false
+            imgName: "mosme13.jpg"
         },
         {
-            title: "",
-            titleDesc: "",
             caption: "Mosme摩斯米 - 線上測驗、練習",
             captionDesc: "",
-            imgName: "mosme14.jpg",
-            class: "",
-            visible: false
+            imgName: "mosme14.jpg"
         },
         {
-            title: "",
-            titleDesc: "",
             caption: "Mosme摩斯米 - 成績報表",
             captionDesc: "",
-            imgName: "mosme15.jpg",
-            class: "",
-            visible: false
+            imgName: "mosme15.jpg"
         }
     ],
     hoogaaTeacher: [{
-            title: "hoogaa(教師端)",
-            titleDesc: "即互動測驗，即時顯示作答與排名，提升競爭和趣味性",
-            caption: "hoogaa(教師端) - 產生PIN碼、等待學生進入",
-            captionDesc: "產生PIN碼提供給學生輸入、檢視當前進入的學生",
-            imgName: "hoogaa-teacher1.jpg",
-            class: "6u",
-            visible: true
-        },
-        {
-            title: "",
-            titleDesc: "",
             caption: "hoogaa(教師端) - 開始考試倒數",
             captionDesc: "教師端統一開始進入競賽倒數",
-            imgName: "hoogaa-teacher2.jpg",
-            class: "",
-            visible: false
+            imgName: "hoogaa-teacher2.jpg"
         },
         {
-            title: "",
-            titleDesc: "",
             caption: "hoogaa(教師端) - 即時競賽結果",
             captionDesc: "即時看考生作答情況、排名",
-            imgName: "hoogaa-teacher3.jpg",
-            class: "",
-            visible: false
+            imgName: "hoogaa-teacher3.jpg"
         },
         {
-            title: "",
-            titleDesc: "",
             caption: "hoogaa(教師端) - 競賽最終結果",
             captionDesc: "顯示考生答題資訊報表、排名及分數",
-            imgName: "hoogaa-teacher4.jpg",
-            class: "",
-            visible: false
+            imgName: "hoogaa-teacher4.jpg"
         },
         {
-            title: "",
-            titleDesc: "",
             caption: "hoogaa(教師端) - 試題解析",
             captionDesc: "顯示試題解析、查看每題學生作答狀況",
-            imgName: "hoogaa-teacher5.jpg",
-            class: "",
-            visible: false
+            imgName: "hoogaa-teacher5.jpg"
         }
     ],
     hoogaaStudent: [{
-            title: "hoogaa(學生端)",
-            titleDesc: "即互動測驗，即時顯示作答與排名，提升競爭和趣味性",
-            caption: "hoogaa(學生端) - 輸入PIN碼",
-            captionDesc: "學生輸入教師端所提供的代碼，即可進入等待競賽畫面",
-            imgName: "hoogaa-student1.jpg",
-            class: "6u",
-            visible: true
-        },
-        {
-            title: "",
-            titleDesc: "",
             caption: "hoogaa(學生端) - 輸入名稱",
             captionDesc: "輸入名稱或備註，供辨別身分",
-            imgName: "hoogaa-student2.jpg",
-            class: "",
-            visible: false
+            imgName: "hoogaa-student2.jpg"
         },
         {
-            title: "",
-            titleDesc: "",
             caption: "hoogaa(學生端) - 等待進入競賽",
             captionDesc: "輸入完基本資料後，進入等待考試畫面，等教師端統一開始競賽",
-            imgName: "hoogaa-student3.jpg",
-            class: "",
-            visible: false
+            imgName: "hoogaa-student3.jpg"
         },
         {
-            title: "",
-            titleDesc: "",
             caption: "hoogaa(學生端) - 作答畫面",
             captionDesc: "開始作答，時間倒計時，作答完或是時間到會顯示正確解答",
-            imgName: "hoogaa-student4.jpg",
-            class: "",
-            visible: false
+            imgName: "hoogaa-student4.jpg"
         },
         {
-            title: "",
-            titleDesc: "",
             caption: "hoogaa(學生端) - 即時排名",
             captionDesc: "學生每作答完一題，會即時顯示當前分數及排名狀況",
-            imgName: "hoogaa-student5.jpg",
-            class: "",
-            visible: false
+            imgName: "hoogaa-student5.jpg"
         },
         {
-            title: "",
-            titleDesc: "",
             caption: "hoogaa(學生端) - 個人競賽結果",
             captionDesc: "全部作答完後顯示競賽結果",
-            imgName: "hoogaa-student6.jpg",
-            class: "",
-            visible: false
+            imgName: "hoogaa-student6.jpg"
         },
         {
-            title: "",
-            titleDesc: "",
             caption: "hoogaa(學生端) - 查看競賽作答歷程",
             captionDesc: "顯示競賽作答歷程，學生可以看到作答時選擇的選項",
-            imgName: "hoogaa-student7.jpg",
-            class: "",
-            visible: false
+            imgName: "hoogaa-student7.jpg"
         }
     ],
     designBack: [{
-        title: "設計資源庫(後台)",
-        titleDesc: "管理和分享雄獅集團各網站元件、商標、圖片等等...",
-        caption: "設計資源庫(後台) - 登入",
-        captionDesc: "登入公司ERP系統帳號密碼",
-        imgName: "designBack1.jpg",
-        class: "6u",
-        visible: true
-    }, {
-        title: "",
-        titleDesc: "",
         caption: "設計資源庫(後台) - 首頁",
         captionDesc: "公告操作(前台可以看到這裡新增的公告)",
-        imgName: "designBack2.jpg",
-        class: "",
-        visible: false
+        imgName: "designBack2.jpg"
     }, {
-        title: "",
-        titleDesc: "",
         caption: "設計資源庫(後台) - 管理員設定",
         captionDesc: "網站類別新增/修改，控制前台選單資料",
-        imgName: "designBack3.jpg",
-        class: "",
-        visible: false
+        imgName: "designBack3.jpg"
     }, {
-        title: "",
-        titleDesc: "",
         caption: "設計資源庫(後台) - 管理員設定",
         captionDesc: "需求單列表，審核前台上傳的圖片，審核通過才可以在前台顯示",
-        imgName: "designBack4.jpg",
-        class: "",
-        visible: false
+        imgName: "designBack4.jpg"
     }, {
-        title: "",
-        titleDesc: "",
         caption: "設計資源庫(後台) - 管理員設定",
         captionDesc: "使用者列表，管理使用者權限設定",
-        imgName: "designBack5.jpg",
-        class: "",
-        visible: false
+        imgName: "designBack5.jpg"
     }, {
-        title: "",
-        titleDesc: "",
         caption: "設計資源庫(後台) - 上架內容列表(商標系統為例)",
         captionDesc: "管理上架內容",
-        imgName: "designBack6.jpg",
-        class: "",
-        visible: false
+        imgName: "designBack6.jpg"
     }, {
-        title: "",
-        titleDesc: "",
         caption: "設計資源庫(後台) - 編輯上架內容",
         captionDesc: "編輯上架內容",
-        imgName: "designBack7.jpg",
-        class: "",
-        visible: false
+        imgName: "designBack7.jpg"
     }],
     designFont: [{
-        title: "設計資源庫(前台)",
-        titleDesc: "管理和分享雄獅集團各網站元件、商標、圖片等等...",
-        caption: "設計資源庫(前台) - 首頁",
-        captionDesc: "可以直接輸入關鍵字查詢元件，或選擇的元件分類",
-        imgName: "designFont1.jpg",
-        class: "6u",
-        visible: true
-    }, {
-        title: "",
-        titleDesc: "",
         caption: "設計資源庫(前台) - 登入",
         captionDesc: "登入集團ERP系統之帳號密碼",
-        imgName: "designFont2.jpg",
-        class: "",
-        visible: false
+        imgName: "designFont2.jpg"
     }, {
-        title: "",
-        titleDesc: "",
         caption: "設計資源庫(前台) - 下載預覽網頁控件",
         captionDesc: "提供使用者下載或預覽網頁控件",
-        imgName: "designFont3.jpg",
-        class: "",
-        visible: false
+        imgName: "designFont3.jpg"
     }, {
-        title: "",
-        titleDesc: "",
         caption: "設計資源庫(前台) - 雄好玩(吉祥物)圖片創建、下載",
         captionDesc: "提供使用者下載雄獅吉祥物圖片、快速創建吉祥物圖入口",
-        imgName: "designFont4.jpg",
-        class: "",
-        visible: false
+        imgName: "designFont4.jpg"
     }, {
-        title: "",
-        titleDesc: "",
         caption: "設計資源庫(前台) - 創建雄好玩(吉祥物)",
         captionDesc: "利用部位圖片組成一張客製化的雄好玩(吉祥物)",
-        imgName: "designFont5.jpg",
-        class: "",
-        visible: false
+        imgName: "designFont5.jpg"
     }, {
-        title: "",
-        titleDesc: "",
         caption: "設計資源庫(前台) - 暫存創建到一半的雄好玩(吉祥物)",
         captionDesc: "暫存使用者所創建的雄好玩圖(吉祥物)，可繼續編輯",
-        imgName: "designFont7.jpg",
-        class: "",
-        visible: false
+        imgName: "designFont7.jpg"
     }, {
-        title: "",
-        titleDesc: "",
         caption: "設計資源庫(前台) - 創建雄好玩(吉祥物)申請審核",
         captionDesc: "審核使用者所創建的雄好玩圖(吉祥物)，才能供人下載",
-        imgName: "designFont8.jpg",
-        class: "",
-        visible: false
+        imgName: "designFont8.jpg"
     }, {
-        title: "",
-        titleDesc: "",
         caption: "設計資源庫(前台) - 創建雄好玩(吉祥物)審核流程",
         captionDesc: "審核使用者所創建的雄好玩圖(吉祥物)，才能供人下載",
-        imgName: "designFont6.jpg",
-        class: "",
-        visible: false
+        imgName: "designFont6.jpg"
     }, {
-        title: "",
-        titleDesc: "",
         caption: "設計資源庫(前台) - 搜尋結果",
-        imgName: "designFont9.jpg",
-        class: "",
-        visible: false
+        captionDesc: "",
+        imgName: "designFont9.jpg"
     }, {
-        title: "",
-        titleDesc: "",
         caption: "設計資源庫(前台) - 訊息通知",
-        imgName: "designFont10.jpg",
-        class: "",
-        visible: false
-    }],
-    works: []
+        captionDesc: "",
+        imgName: "designFont10.jpg"
+    }]
 }
