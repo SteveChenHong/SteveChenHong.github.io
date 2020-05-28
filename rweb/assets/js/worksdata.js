b268dbf77f936445a3a915b32778158879bb2cdd
user <user@DESKTOP-BL5257G>
2020-05-28 23:37:39 +0800

    update

--- rweb/assets/js/worksdata.js
+++ rweb/assets/js/worksdata.js
@@ -250,47 +250,47 @@ var worksdata = {
         desc: "旅遊產品購買、報名",
         caption: "雄保旅遊網 - 首頁",
         captionDesc: "",
-        imgName: "lionbobby1.jpg",
+        imgName: "lionbobby1.png",
     }, {
         caption: "雄保旅遊網 - 搜尋結果頁",
         captionDesc: "顯示產品列表、包含月曆選團、比較、加入收藏、看過紀錄等功能",
-        imgName: "lionbobby2.jpg"
+        imgName: "lionbobby2.png"
     }, {
         caption: "雄保旅遊網 - 行程介紹頁1",
         captionDesc: "顯示產品月曆、產品詳細資訊",
-        imgName: "lionbobby3.jpg"
+        imgName: "lionbobby3.png"
     }, {
         caption: "雄保旅遊網 - 行程介紹頁2",
         captionDesc: "顯示行程介紹、注意事項等內容",
-        imgName: "lionbobby4.jpg"
+        imgName: "lionbobby4.png"
     }, {
         caption: "雄保旅遊網 - 選擇參團人數",
         captionDesc: "選擇人數並確認旅遊契約書等須知訊息",
-        imgName: "lionbobby5.jpg"
+        imgName: "lionbobby5.png"
     }, {
         caption: "雄保旅遊網 - 填寫旅客資料",
         captionDesc: "",
-        imgName: "lionbobby6.jpg"
+        imgName: "lionbobby6.png"
     }, {
         caption: "雄保旅遊網 - 訂單明細",
         captionDesc: "",
-        imgName: "lionbobby7.jpg"
+        imgName: "lionbobby7.png"
     }, {
         caption: "雄保旅遊網 - 訂單列表",
         captionDesc: "",
-        imgName: "lionbobby8.jpg"
+        imgName: "lionbobby8.png"
     }, {
         caption: "雄保旅遊網 - 我的收藏列表",
         captionDesc: "顯示所收藏的產品",
-        imgName: "lionbobby9.jpg"
+        imgName: "lionbobby9.png"
     }, {
         caption: "雄保旅遊網 - 產品比較",
         captionDesc: "比較最多三項產品差異",
-        imgName: "lionbobby10.jpg"
+        imgName: "lionbobby10.png"
     }, {
         caption: "雄保旅遊網 - 瀏覽紀錄",
         captionDesc: "顯示瀏覽過的產品紀錄",
-        imgName: "lionbobby11.jpg"
+        imgName: "lionbobby11.png"
     }],
     hoogaaStudent: [{
         first: true,
@@ -298,36 +298,36 @@ var worksdata = {
         desc: "即互動測驗，即時顯示作答與排名，提升競爭和趣味性",
         caption: "hoogaa(學生端) - 輸入PIN碼",
         captionDesc: "學生輸入教師端所提供的代碼，即可進入等待競賽畫面",
-        imgName: "hoogaa-student1.jpg",
+        imgName: "hoogaa-student1.png",
     },{
         caption: "hoogaa(學生端) - 輸入名稱",
         captionDesc: "輸入名稱或備註，供辨別身分",
-        imgName: "hoogaa-student2.jpg"
+        imgName: "hoogaa-student2.png"
     },
     {
         caption: "hoogaa(學生端) - 等待進入競賽",
         captionDesc: "輸入完基本資料後，進入等待考試畫面，等教師端統一開始競賽",
-        imgName: "hoogaa-student3.jpg"
+        imgName: "hoogaa-student3.png"
     },
     {
         caption: "hoogaa(學生端) - 作答畫面",
         captionDesc: "開始作答，時間倒計時，作答完或是時間到會顯示正確解答",
-        imgName: "hoogaa-student4.jpg"
+        imgName: "hoogaa-student4.png"
     },
     {
         caption: "hoogaa(學生端) - 即時排名",
         captionDesc: "學生每作答完一題，會即時顯示當前分數及排名狀況",
-        imgName: "hoogaa-student5.jpg"
+        imgName: "hoogaa-student5.png"
     },
     {
         caption: "hoogaa(學生端) - 個人競賽結果",
         captionDesc: "全部作答完後顯示競賽結果",
-        imgName: "hoogaa-student6.jpg"
+        imgName: "hoogaa-student6.png"
     },
     {
         caption: "hoogaa(學生端) - 查看競賽作答歷程",
         captionDesc: "顯示競賽作答歷程，學生可以看到作答時選擇的選項",
-        imgName: "hoogaa-student7.jpg"
+        imgName: "hoogaa-student7.png"
     }
     ],
     hoogaaTeacher: [{
@@ -336,26 +336,26 @@ var worksdata = {
         desc: "即互動測驗，即時顯示作答與排名，提升競爭和趣味性",
         caption: "hoogaa(教師端) - 產生PIN碼、等待學生進入",
         captionDesc: "產生PIN碼提供給學生輸入、檢視當前進入的學生",
-        imgName: "hoogaa-teacher1.jpg",
+        imgName: "hoogaa-teacher1.png",
     },{
         caption: "hoogaa(教師端) - 開始考試倒數",
         captionDesc: "教師端統一開始進入競賽倒數",
-        imgName: "hoogaa-teacher2.jpg"
+        imgName: "hoogaa-teacher2.png"
     },
     {
         caption: "hoogaa(教師端) - 即時競賽結果",
         captionDesc: "即時看考生作答情況、排名",
-        imgName: "hoogaa-teacher3.jpg"
+        imgName: "hoogaa-teacher3.png"
     },
     {
         caption: "hoogaa(教師端) - 競賽最終結果",
         captionDesc: "顯示考生答題資訊報表、排名及分數",
-        imgName: "hoogaa-teacher4.jpg"
+        imgName: "hoogaa-teacher4.png"
     },
     {
         caption: "hoogaa(教師端) - 試題解析",
         captionDesc: "顯示試題解析、查看每題學生作答狀況",
-        imgName: "hoogaa-teacher5.jpg"
+        imgName: "hoogaa-teacher5.png"
     }
     ],
     join: [{
@@ -364,41 +364,41 @@ var worksdata = {
         desc: "提供旅天下的加盟商使用的後台系統",
         caption: "加盟平台 - 登入畫面",
         captionDesc: "輸入帳號密碼、一次性密碼(OTP)進行登入",
-        imgName: "join1.jpg",
+        imgName: "join1.png",
     }, {
         caption: "加盟平台 - 首頁",
         captionDesc: "查看最新系統留言、訂單留言、其他登入者資訊",
-        imgName: "join2.jpg"
+        imgName: "join2.png"
     }, {
         caption: "加盟平台 - 團體銷售",
         captionDesc: "顯示團體產品列表",
-        imgName: "join3.jpg"
+        imgName: "join3.png"
     }, {
         caption: "加盟平台 - 確認產品",
         captionDesc: "確認選購的產品、選擇參團人數",
-        imgName: "join6.jpg"
+        imgName: "join6.png"
     }, {
         caption: "加盟平台 - 填寫旅客資料",
         captionDesc: "填寫旅客資料",
-        imgName: "join7.jpg"
+        imgName: "join7.png"
     }, {
         caption: "加盟平台 - 訂單列表",
         captionDesc: "查看訂單列表",
-        imgName: "join4.jpg"
+        imgName: "join4.png"
     }, {
 
         caption: "加盟平台 - 訂單明細",
         captionDesc: "查看訂單明細、處理訂單相關功能",
-        imgName: "join8.jpg"
+        imgName: "join8.png"
     },
     {
         caption: "加盟平台 - 門店應付",
         captionDesc: "查看門店應付帳款等資訊",
-        imgName: "join9.jpg"
+        imgName: "join9.png"
     }, {
         caption: "加盟平台 - 收退款審核",
         captionDesc: "處理收退款審核作業",
-        imgName: "join5.jpg"
+        imgName: "join5.png"
     }
     ],
     mosme: [{
@@ -407,76 +407,76 @@ var worksdata = {
         desc: "教材題庫整合、測驗或練習、自訂組卷、班級管理等等",
         caption: "Mosme摩斯米 - 首頁",
         captionDesc: "輸入關鍵字可快速搜尋相關書籍資料",
-        imgName: "mosme1.jpg",
+        imgName: "mosme1.png",
     },{
         caption: "Mosme摩斯米 - 書籍列表",
         captionDesc: "顯示書籍產品",
-        imgName: "mosme2.jpg"
+        imgName: "mosme2.png"
     },
     {
         caption: "Mosme摩斯米 - 書籍詳細資料",
         captionDesc: "顯示書籍簡介、題庫練習功能、書籍開通功能",
-        imgName: "mosme3.jpg"
+        imgName: "mosme3.png"
     },
     {
         caption: "Mosme摩斯米 - 我的書單",
         captionDesc: "顯示個人開通過的書籍",
-        imgName: "mosme4.jpg"
+        imgName: "mosme4.png"
     },
     {
         caption: "Mosme摩斯米 - 開通審核",
         captionDesc: "提供業務處理書籍開通、查詢開通紀錄等功能",
-        imgName: "mosme5.jpg"
+        imgName: "mosme5.png"
     },
     {
         caption: "Mosme摩斯米 - 課程管理",
         captionDesc: "提供教師對課程綁定相關書籍，將學生加入課程、發布公告、上傳教材、組卷等功能",
-        imgName: "mosme6.jpg"
+        imgName: "mosme6.png"
     },
     {
         caption: "Mosme摩斯米 - 組卷流程1",
         captionDesc: "選擇組卷題庫",
-        imgName: "mosme7.jpg"
+        imgName: "mosme7.png"
     },
     {
         caption: "Mosme摩斯米 - 組卷流程2",
         captionDesc: "選擇組卷考試範圍",
-        imgName: "mosme8.jpg"
+        imgName: "mosme8.png"
     },
     {
         caption: "Mosme摩斯米 - 組卷流程3",
         captionDesc: "選擇組卷題型、題數、配分",
-        imgName: "mosme9.jpg"
+        imgName: "mosme9.png"
     },
     {
         caption: "Mosme摩斯米 - 組卷流程4",
         captionDesc: "選擇組卷章節配題目",
-        imgName: "mosme10.jpg"
+        imgName: "mosme10.png"
     },
     {
         caption: "Mosme摩斯米 - 組卷流程5",
         captionDesc: "設定試卷詳細資料",
-        imgName: "mosme11.jpg"
+        imgName: "mosme11.png"
     },
     {
         caption: "Mosme摩斯米 - 題目預覽",
         captionDesc: "",
-        imgName: "mosme12.jpg"
+        imgName: "mosme12.png"
     },
     {
         caption: "Mosme摩斯米 - 匯出試卷",
         captionDesc: "",
-        imgName: "mosme13.jpg"
+        imgName: "mosme13.png"
     },
     {
         caption: "Mosme摩斯米 - 線上測驗、練習",
         captionDesc: "",
-        imgName: "mosme14.jpg"
+        imgName: "mosme14.png"
     },
     {
         caption: "Mosme摩斯米 - 成績報表",
         captionDesc: "",
-        imgName: "mosme15.jpg"
+        imgName: "mosme15.png"
     }
     ],
     designFont: [{
@@ -485,43 +485,43 @@ var worksdata = {
         desc: "管理和分享雄獅集團各網站元件、商標、圖片等等...",
         caption: "設計資源庫(前台) - 首頁",
         captionDesc: "可以直接輸入關鍵字查詢元件，或選擇的元件分類",
-        imgName: "designFont1.jpg",
+        imgName: "designFont1.png",
     },{
         caption: "設計資源庫(前台) - 登入",
         captionDesc: "登入集團ERP系統之帳號密碼",
-        imgName: "designFont2.jpg"
+        imgName: "designFont2.png"
     }, {
         caption: "設計資源庫(前台) - 下載預覽網頁控件",
         captionDesc: "提供使用者下載或預覽網頁控件",
-        imgName: "designFont3.jpg"
+        imgName: "designFont3.png"
     }, {
         caption: "設計資源庫(前台) - 雄好玩(吉祥物)圖片創建、下載",
         captionDesc: "提供使用者下載雄獅吉祥物圖片、快速創建吉祥物圖入口",
-        imgName: "designFont4.jpg"
+        imgName: "designFont4.png"
     }, {
         caption: "設計資源庫(前台) - 創建雄好玩(吉祥物)",
         captionDesc: "利用部位圖片組成一張客製化的雄好玩(吉祥物)",
-        imgName: "designFont5.jpg"
+        imgName: "designFont5.png"
     }, {
         caption: "設計資源庫(前台) - 暫存創建到一半的雄好玩(吉祥物)",
         captionDesc: "暫存使用者所創建的雄好玩圖(吉祥物)，可繼續編輯",
-        imgName: "designFont7.jpg"
+        imgName: "designFont7.png"
     }, {
         caption: "設計資源庫(前台) - 創建雄好玩(吉祥物)申請審核",
         captionDesc: "審核使用者所創建的雄好玩圖(吉祥物)，才能供人下載",
-        imgName: "designFont8.jpg"
+        imgName: "designFont8.png"
     }, {
         caption: "設計資源庫(前台) - 創建雄好玩(吉祥物)審核流程",
         captionDesc: "審核使用者所創建的雄好玩圖(吉祥物)，才能供人下載",
-        imgName: "designFont6.jpg"
+        imgName: "designFont6.png"
     }, {
         caption: "設計資源庫(前台) - 搜尋結果",
         captionDesc: "",
-        imgName: "designFont9.jpg"
+        imgName: "designFont9.png"
     }, {
         caption: "設計資源庫(前台) - 訊息通知",
         captionDesc: "",
-        imgName: "designFont10.jpg"
+        imgName: "designFont10.png"
     }],
     designBack: [{
         first: true,
@@ -529,30 +529,30 @@ var worksdata = {
         desc: "管理和分享雄獅集團各網站元件、商標、圖片等等...",
         caption: "設計資源庫(後台) - 登入",
         captionDesc: "登入公司ERP系統帳號密碼",
-        imgName: "designBack1.jpg",
+        imgName: "designBack1.png",
     },{
         caption: "設計資源庫(後台) - 首頁",
         captionDesc: "公告操作(前台可以看到這裡新增的公告)",
-        imgName: "designBack2.jpg"
+        imgName: "designBack2.png"
     }, {
         caption: "設計資源庫(後台) - 管理員設定",
         captionDesc: "網站類別新增/修改，控制前台選單資料",
-        imgName: "designBack3.jpg"
+        imgName: "designBack3.png"
     }, {
         caption: "設計資源庫(後台) - 管理員設定",
         captionDesc: "需求單列表，審核前台上傳的圖片，審核通過才可以在前台顯示",
-        imgName: "designBack4.jpg"
+        imgName: "designBack4.png"
     }, {
         caption: "設計資源庫(後台) - 管理員設定",
         captionDesc: "使用者列表，管理使用者權限設定",
-        imgName: "designBack5.jpg"
+        imgName: "designBack5.png"
     }, {
         caption: "設計資源庫(後台) - 上架內容列表(商標系統為例)",
         captionDesc: "管理上架內容",
-        imgName: "designBack6.jpg"
+        imgName: "designBack6.png"
     }, {
         caption: "設計資源庫(後台) - 編輯上架內容",
         captionDesc: "編輯上架內容",
-        imgName: "designBack7.jpg"
+        imgName: "designBack7.png"
     }],
 }
