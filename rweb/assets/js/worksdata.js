e29a2afedd18f4802e4ba6fbbf4844a1c9687abe
user <user@DESKTOP-BL5257G>
2022-09-03 21:11:31 +0800

    add rweb

---
+++ rweb/assets/js/worksdata.js
@@ -0,0 +1,286 @@
+var worksdata = {
+    lifetour: [{
+        first: true,
+        title: "五福旅遊網",
+        desc: "提供豐富團體旅遊資訊、線上購買跟團",
+        caption: "五福旅遊網 - 搜尋結果頁1",
+        captionDesc: "顯示產品列表、條件搜尋、進階篩選等功能",
+        imgName: "lifetour1.png",
+    }, {
+        caption: "五福旅遊網 - 搜尋結果頁2",
+        captionDesc: "顯示產品列表、條件搜尋、進階篩選等功能",
+        imgName: "lifetour2.png"
+    }, {
+        caption: "五福旅遊網 - 搜尋結果頁3",
+        captionDesc: "顯示產品列表、條件搜尋、進階篩選等功能",
+        imgName: "lifetour2-2.png"
+    }, {
+        caption: "五福旅遊網 - 搜尋結果頁3(手機版)",
+        captionDesc: "顯示產品列表、條件搜尋、進階篩選等功能",
+        imgName: "lifetour3.png"
+    }, {
+        caption: "五福旅遊網 - 產品明細頁1",
+        captionDesc: "顯示團體產品詳細資料，有航班資訊、每日行程、產品特色、價格、須知等，包含月曆切換選擇其他團產、社群分享等功能",
+        imgName: "lifetour4.png"
+    }, {
+        caption: "五福旅遊網 - 產品明細頁2",
+        captionDesc: "顯示團體產品詳細資料，有航班資訊、每日行程、產品特色、價格、須知等，包含月曆切換選擇其他團產、社群分享等功能",
+        imgName: "lifetour5.png"
+    }, {
+        caption: "五福旅遊網 - 產品明細頁3",
+        captionDesc: "顯示團體產品詳細資料，有航班資訊、每日行程、產品特色、價格、須知等，包含月曆切換選擇其他團產、社群分享等功能",
+        imgName: "lifetour6.png"
+    }, {
+        caption: "五福旅遊網 - 產品明細頁4(手機版)",
+        captionDesc: "",
+        imgName: "lifetour7.png"
+    }, {
+        caption: "五福旅遊網 - 產品明細列印頁1",
+        captionDesc: "列印、下載PDF、下載Word(只限內網)等功能",
+        imgName: "lifetour20.png"
+    }, {
+        caption: "五福旅遊網 - 產品明細列印頁2",
+        captionDesc: "列印、下載PDF、下載Word(只限內網)等功能",
+        imgName: "lifetour21.png"
+    }, {
+        caption: "五福旅遊網 - 選擇旅客數量1",
+        captionDesc: "選擇旅客數量",
+        imgName: "lifetour8.png"
+    }, {
+        caption: "五福旅遊網 - 選擇旅客數量2",
+        captionDesc: "選擇旅客數量",
+        imgName: "lifetour9.png"
+    }, {
+        caption: "五福旅遊網 - 選擇旅客數量3",
+        captionDesc: "選擇旅客數量",
+        imgName: "lifetour10.png"
+    }, {
+        caption: "五福旅遊網 - 填寫旅客資料1",
+        captionDesc: "填寫旅客資料",
+        imgName: "lifetour11.png"
+    }, {
+        caption: "五福旅遊網 - 填寫旅客資料2",
+        captionDesc: "填寫旅客資料",
+        imgName: "lifetour12.png"
+    }, {
+        caption: "五福旅遊網 - 選擇付款方式1",
+        captionDesc: "選擇付款方式",
+        imgName: "lifetour13.png"
+    }, {
+        caption: "五福旅遊網 - 選擇付款方式2",
+        captionDesc: "選擇付款方式",
+        imgName: "lifetour14.png"
+    }, {
+        caption: "五福旅遊網 - 線上刷卡頁1",
+        captionDesc: "線上刷卡頁",
+        imgName: "lifetour15.png"
+    }, {
+        caption: "五福旅遊網 - 線上刷卡頁2",
+        captionDesc: "線上刷卡頁",
+        imgName: "lifetour16.png"
+    }, {
+        caption: "五福旅遊網 - 線上刷卡頁3",
+        captionDesc: "線上刷卡頁",
+        imgName: "lifetour17.png"
+    }, {
+        caption: "五福旅遊網 - 線上刷卡頁4",
+        captionDesc: "線上刷卡頁",
+        imgName: "lifetour18.png"
+    }, {
+        caption: "五福旅遊網 - 線上刷卡頁5(付款完成)",
+        captionDesc: "線上刷卡頁",
+        imgName: "lifetour19.png"
+    }],
+    lionbobby: [{
+        first: true,
+        title: "雄保旅遊網",
+        desc: "提供豐富團體旅遊資訊、線上購買跟團",
+        caption: "雄保旅遊網 - 首頁",
+        captionDesc: "",
+        imgName: "lionbobby1.jpg",
+    }, {
+        caption: "雄保旅遊網 - 搜尋結果頁",
+        captionDesc: "顯示產品列表、包含月曆選團、比較、加入收藏、看過紀錄等功能",
+        imgName: "lionbobby2.jpg"
+    }, {
+        caption: "雄保旅遊網 - 行程介紹頁1",
+        captionDesc: "顯示產品月曆、產品詳細資訊",
+        imgName: "lionbobby3.jpg"
+    }, {
+        caption: "雄保旅遊網 - 行程介紹頁2",
+        captionDesc: "顯示行程介紹、注意事項等內容",
+        imgName: "lionbobby4.jpg"
+    }, {
+        caption: "雄保旅遊網 - 選擇參團人數",
+        captionDesc: "選擇人數並確認旅遊契約書等須知訊息",
+        imgName: "lionbobby5.jpg"
+    }, {
+        caption: "雄保旅遊網 - 填寫旅客資料",
+        captionDesc: "",
+        imgName: "lionbobby6.jpg"
+    }, {
+        caption: "雄保旅遊網 - 訂單明細",
+        captionDesc: "",
+        imgName: "lionbobby7.jpg"
+    }, {
+        caption: "雄保旅遊網 - 訂單列表",
+        captionDesc: "",
+        imgName: "lionbobby8.jpg"
+    }, {
+        caption: "雄保旅遊網 - 我的收藏列表",
+        captionDesc: "顯示所收藏的產品",
+        imgName: "lionbobby9.jpg"
+    }, {
+        caption: "雄保旅遊網 - 產品比較",
+        captionDesc: "比較最多三項產品差異",
+        imgName: "lionbobby10.jpg"
+    }, {
+        caption: "雄保旅遊網 - 瀏覽紀錄",
+        captionDesc: "顯示瀏覽過的產品紀錄",
+        imgName: "lionbobby11.jpg"
+    }],
+    HoogaaStudent: [{
+        first: true,
+        title: "Hoogaa 答題競賽互動網(學生端)",
+        desc: "即互動測驗，即時顯示作答與排名，提升競爭和趣味性",
+        caption: "Hoogaa(學生端) - 輸入PIN碼",
+        captionDesc: "學生輸入教師端所提供的代碼，即可進入等待競賽畫面",
+        imgName: "Hoogaa-student1.jpg",
+    }, {
+        caption: "Hoogaa(學生端) - 輸入名稱",
+        captionDesc: "輸入名稱或備註，供辨別身分",
+        imgName: "Hoogaa-student2.jpg"
+    },
+    {
+        caption: "Hoogaa(學生端) - 等待進入競賽",
+        captionDesc: "輸入完基本資料後，進入等待考試畫面，等教師端統一開始競賽",
+        imgName: "Hoogaa-student3.jpg"
+    },
+    {
+        caption: "Hoogaa(學生端) - 作答畫面",
+        captionDesc: "開始作答，時間倒計時，作答完或是時間到會顯示正確解答",
+        imgName: "Hoogaa-student4.jpg"
+    },
+    {
+        caption: "Hoogaa(學生端) - 即時排名",
+        captionDesc: "學生每作答完一題，會即時顯示當前分數及排名狀況",
+        imgName: "Hoogaa-student5.jpg"
+    },
+    {
+        caption: "Hoogaa(學生端) - 個人競賽結果",
+        captionDesc: "全部作答完後顯示競賽結果",
+        imgName: "Hoogaa-student6.jpg"
+    },
+    {
+        caption: "Hoogaa(學生端) - 查看競賽作答歷程",
+        captionDesc: "顯示競賽作答歷程，學生可以看到作答時選擇的選項",
+        imgName: "Hoogaa-student7.jpg"
+    }
+    ],
+    HoogaaTeacher: [{
+        first: true,
+        title: "Hoogaa 答題競賽互動網(教師端)",
+        desc: "即互動測驗，即時顯示作答與排名，提升競爭和趣味性",
+        caption: "Hoogaa(教師端) - 產生PIN碼、等待學生進入",
+        captionDesc: "產生PIN碼提供給學生輸入、檢視當前進入的學生",
+        imgName: "Hoogaa-teacher1.jpg",
+    }, {
+        caption: "Hoogaa(教師端) - 開始考試倒數",
+        captionDesc: "教師端統一開始進入競賽倒數",
+        imgName: "Hoogaa-teacher2.jpg"
+    },
+    {
+        caption: "Hoogaa(教師端) - 即時競賽結果",
+        captionDesc: "即時看考生作答情況、排名",
+        imgName: "Hoogaa-teacher3.jpg"
+    },
+    {
+        caption: "Hoogaa(教師端) - 競賽最終結果",
+        captionDesc: "顯示考生答題資訊報表、排名及分數",
+        imgName: "Hoogaa-teacher4.jpg"
+    },
+    {
+        caption: "Hoogaa(教師端) - 試題解析",
+        captionDesc: "顯示試題解析、查看每題學生作答狀況",
+        imgName: "Hoogaa-teacher5.jpg"
+    }
+    ],
+    mosme: [{
+        first: true,
+        title: "MOSME摩斯米 - 題庫測評網",
+        desc: "教材題庫整合、測驗或練習、自訂組卷、班級管理等等",
+        caption: "MOSME摩斯米 - 首頁",
+        captionDesc: "輸入關鍵字可快速搜尋相關書籍資料",
+        imgName: "mosme1.jpg",
+    }, {
+        caption: "MOSME摩斯米 - 書籍列表",
+        captionDesc: "顯示書籍產品",
+        imgName: "mosme2.jpg"
+    },
+    {
+        caption: "MOSME摩斯米 - 書籍詳細資料",
+        captionDesc: "顯示書籍簡介、題庫練習功能、書籍開通功能",
+        imgName: "mosme3.jpg"
+    },
+    {
+        caption: "MOSME摩斯米 - 我的書單",
+        captionDesc: "顯示個人開通過的書籍",
+        imgName: "mosme4.jpg"
+    },
+    {
+        caption: "MOSME摩斯米 - 開通審核",
+        captionDesc: "提供業務處理書籍開通、查詢開通紀錄等功能",
+        imgName: "mosme5.jpg"
+    },
+    {
+        caption: "MOSME摩斯米 - 課程管理",
+        captionDesc: "提供教師對課程綁定相關書籍，將學生加入課程、發布公告、上傳教材、組卷等功能",
+        imgName: "mosme6.jpg"
+    },
+    {
+        caption: "MOSME摩斯米 - 組卷流程1",
+        captionDesc: "選擇組卷題庫",
+        imgName: "mosme7.jpg"
+    },
+    {
+        caption: "MOSME摩斯米 - 組卷流程2",
+        captionDesc: "選擇組卷考試範圍",
+        imgName: "mosme8.jpg"
+    },
+    {
+        caption: "MOSME摩斯米 - 組卷流程3",
+        captionDesc: "選擇組卷題型、題數、配分",
+        imgName: "mosme9.jpg"
+    },
+    {
+        caption: "MOSME摩斯米 - 組卷流程4",
+        captionDesc: "選擇組卷章節配題目",
+        imgName: "mosme10.jpg"
+    },
+    {
+        caption: "MOSME摩斯米 - 組卷流程5",
+        captionDesc: "設定試卷詳細資料",
+        imgName: "mosme11.jpg"
+    },
+    {
+        caption: "MOSME摩斯米 - 題目預覽",
+        captionDesc: "",
+        imgName: "mosme12.jpg"
+    },
+    {
+        caption: "MOSME摩斯米 - 匯出試卷",
+        captionDesc: "",
+        imgName: "mosme13.jpg"
+    },
+    {
+        caption: "MOSME摩斯米 - 線上測驗、練習",
+        captionDesc: "",
+        imgName: "mosme14.jpg"
+    },
+    {
+        caption: "MOSME摩斯米 - 成績報表",
+        captionDesc: "",
+        imgName: "mosme15.jpg"
+    }
+    ],
+}
